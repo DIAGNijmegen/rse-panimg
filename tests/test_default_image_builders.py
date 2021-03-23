7ac3f59d32484c0a8b348848de683b1d72793c4d
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2021-03-23 09:03:27 +0100

    Adds test for unconsumed files

--- tests/test_default_image_builders.py
+++ tests/test_default_image_builders.py
@@ -2,6 +2,8 @@ import pytest
 
 from panimg.exceptions import UnconsumedFilesException
 from panimg.image_builders import DEFAULT_IMAGE_BUILDERS
+from panimg.panimg import _build_files
+from tests import RESOURCE_PATH
 
 EXPECTED_ERROR_MESSAGE = {
     "image_builder_dicom": [
@@ -35,3 +37,24 @@ def test_image_builder_raises_unconsumed_file_exception(tmp_path, builder):
     assert {**e.value.file_errors} == {
         f: EXPECTED_ERROR_MESSAGE[builder.__name__]
     }
+
+
+@pytest.mark.parametrize("builder", DEFAULT_IMAGE_BUILDERS)
+def test_one_image_consumed_by_each_builder(tmp_path, builder):
+    # Each of these images should only be consumed by one builder
+    # The other files should error out
+    files = {
+        *(RESOURCE_PATH / "dicom").glob("*.dcm"),
+        RESOURCE_PATH / "test_rgb.png",
+        RESOURCE_PATH / "image10x10x10.mha",
+        RESOURCE_PATH / "image10x11x12.nii.gz",
+        RESOURCE_PATH / "valid_tiff.tif",
+    }
+
+    result = _build_files(
+        builder=builder, files=files, output_directory=tmp_path
+    )
+
+    assert len(result.new_images) == 1
+    assert len(result.consumed_files) == len(files) - len(result.file_errors)
+    assert result.consumed_files | {*result.file_errors} == files
