46b9ff90b6f10cc6cb1c3152a7f2ed59679e55e1
nlessmann <773597+nlessmann@users.noreply.github.com>
2021-03-23 12:18:38 +0100

    Add mock module to defer import errors

--- panimg/exceptions.py
+++ panimg/exceptions.py
@@ -16,3 +16,22 @@ class UnconsumedFilesException(Exception):
     def __init__(self, *args, file_errors: Dict[Path, List[str]]):
         super().__init__(*args)
         self.file_errors = file_errors
+
+
+class MissingLibraryException(RuntimeError):
+    pass
+
+
+class MissingLibraryMockModule:
+    """
+    Replaces a module that is not working due to a missing external
+    dependency (such as OpenSlide) and raises an exception as soon as
+    the module is used for the first time (i.e., when an object in the
+    module is accessed).
+    """
+
+    def __init__(self, *args):
+        self.args = args
+
+    def __getattr__(self, item):
+        raise MissingLibraryException(*self.args)
