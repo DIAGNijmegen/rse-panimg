acda9f4381bc7a6aeab4d80d85ce036737f1855f
amickan <amickan1990@gmail.com>
2021-05-05 16:12:57 +0200

    added spacing information for fds, fda, and e2e

--- panimg/image_builders/oct.py
+++ panimg/image_builders/oct.py
@@ -4,7 +4,13 @@ from typing import DefaultDict, Iterator, List, Set
 
 import SimpleITK
 import numpy as np
-from construct.core import StreamError
+from construct.core import (
+    Float64l,
+    Int8ul,
+    PaddedString,
+    StreamError,
+    Struct,
+)
 from oct_converter.readers import E2E, FDA, FDS
 
 from panimg.exceptions import UnconsumedFilesException, ValidationError
@@ -15,12 +21,38 @@ def format_error(message: str) -> str:
     return f"OCT image builder: {message}"
 
 
-def create_itk_oct_volume(file, volume):
+def create_itk_oct_volume(file, volume, oct_voxel_spacing):
     img_array = np.array(volume)
     img = SimpleITK.GetImageFromArray(img_array, isVector=False)
-    return SimpleITKImage(
-        image=img, name=file.name, consumed_files={file}, spacing_valid=False,
+    [st, _, sl] = img_array.shape
+    img.SetSpacing(
+        [
+            oct_voxel_spacing.xmm / st,
+            oct_voxel_spacing.y / 1000,
+            oct_voxel_spacing.zmm / sl,
+        ]
     )
+    return SimpleITKImage(
+        image=img, name=file.name, consumed_files={file}, spacing_valid=True,
+    )
+
+
+def extract_voxel_spacing(img):
+    voxel_spacing_meta_data = Struct(
+        "unknown" / PaddedString(12, "utf16"),
+        "xmm" / Float64l,
+        "zmm" / Float64l,
+        "y" / Float64l,
+        "unknown6" / Float64l,
+        "unknown7" / Float64l,
+        "unknown8" / Int8ul,
+    )
+    with open(img.filepath, "rb") as f:
+        chunk_location, chunk_size = img.chunk_dict[b"@PARAM_SCAN_04"]
+        f.seek(chunk_location)
+        raw = f.read(chunk_size)
+        oct_voxel_spacing = voxel_spacing_meta_data.parse(raw)
+    return oct_voxel_spacing
 
 
 def image_builder_oct(*, files: Set[Path]) -> Iterator[SimpleITKImage]:
@@ -48,19 +80,29 @@ def image_builder_oct(*, files: Set[Path]) -> Iterator[SimpleITKImage]:
         try:
             if file.suffix == ".fds":
                 img = FDS(file)
+                oct_voxel_spacing = extract_voxel_spacing(img)
             elif file.suffix == ".fda":
                 img = FDA(file)
+                oct_voxel_spacing = extract_voxel_spacing(img)
             elif file.suffix == ".e2e":
                 img = E2E(file)
+                oct_voxel_spacing.xmm = 6
+                oct_voxel_spacing.zmm = 4.5
+                oct_voxel_spacing.y = 3.9
             else:
                 raise ValueError
 
             oct_volume = img.read_oct_volume()
+
             if file.suffix == ".e2e":
                 for volume in oct_volume:
-                    yield create_itk_oct_volume(file, volume.volume)
+                    yield create_itk_oct_volume(
+                        file, volume.volume, oct_voxel_spacing
+                    )
             else:
-                yield create_itk_oct_volume(file, oct_volume.volume)
+                yield create_itk_oct_volume(
+                    file, oct_volume.volume, oct_voxel_spacing
+                )
 
         except (OSError, ValidationError, StreamError, ValueError, IndexError):
             file_errors[file].append(
