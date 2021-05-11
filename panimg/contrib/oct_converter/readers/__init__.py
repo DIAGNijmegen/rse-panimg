b21c66e388bef6572d3a2be790a59cfad4c95b1d
James Meakin <12661555+jmsmkn@users.noreply.github.com>
2021-05-11 13:38:11 +0200

    Removes unused code from oct-converter

--- panimg/contrib/oct_converter/readers/__init__.py
+++ panimg/contrib/oct_converter/readers/__init__.py
@@ -1,7 +1,5 @@
 from .fds import FDS
-from .img import IMG
 from .e2e import E2E
-from .dcm import Dicom
 from .fda import FDA
 
-__all__ = ["FDS", "IMG", "E2E", "Dicom", "FDA"]
+__all__ = ["FDS", "E2E", "FDA"]
