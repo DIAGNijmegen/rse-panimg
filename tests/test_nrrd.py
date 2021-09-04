aebb2dd966a2558e2675b380c5d0ef5a2ca064ca
nlessmann <773597+nlessmann@users.noreply.github.com>
2021-09-04 13:46:11 +0200

    Add NRRD image reader

--- tests/test_nrrd.py
+++ tests/test_nrrd.py
@@ -13,28 +13,18 @@ from tests import RESOURCE_PATH
 
 
 @pytest.mark.parametrize(
-    "src",
+    "srcfile",
     (
-        [RESOURCE_PATH / "nrrd" / "image10x11x12.nrrd"],
-        [RESOURCE_PATH / "nrrd" / "image10x11x12_compressed.nrrd"],
-        [
-            RESOURCE_PATH / "nrrd" / "image10x11x12.nhdr",
-            RESOURCE_PATH / "nrrd" / "image10x11x12.raw",
-        ],
-        [
-            RESOURCE_PATH / "nrrd" / "image10x11x12_compressed.nhdr",
-            RESOURCE_PATH / "nrrd" / "image10x11x12_compressed.raw.gz",
-        ],
+        RESOURCE_PATH / "nrrd" / "image10x11x12.nrrd",
+        RESOURCE_PATH / "nrrd" / "image10x11x12_compressed.nrrd",
     ),
 )
-def test_image_builder_nrrd(tmpdir_factory, src: List[Path]):
+def test_image_builder_nrrd(tmpdir_factory, srcfile: Path):
     dest = Path(tmpdir_factory.mktemp("input"))
 
     # Copy files into an empty temporary directory
-    files = set()
-    for srcfile in src:
-        shutil.copy(srcfile, dest)
-        files.add(dest / srcfile.name)
+    shutil.copy(srcfile, dest)
+    files = {dest / srcfile.name}
 
     # Run image builder
     result = _build_files(
@@ -55,6 +45,54 @@ def test_image_builder_nrrd(tmpdir_factory, src: List[Path]):
     assert image.voxel_width_mm == pytest.approx(1.0)
     assert image.voxel_height_mm == pytest.approx(2.0)
     assert image.voxel_depth_mm == pytest.approx(3.0)
+
+
+@pytest.mark.parametrize(
+    "srcfiles",
+    (
+        [
+            RESOURCE_PATH / "nrrd" / "image10x11x12.nhdr",
+            RESOURCE_PATH / "nrrd" / "image10x11x12.raw",
+        ],
+        [
+            RESOURCE_PATH / "nrrd" / "image10x11x12_compressed.nhdr",
+            RESOURCE_PATH / "nrrd" / "image10x11x12_compressed.raw.gz",
+        ],
+    ),
+)
+def test_image_builder_nrrd_detached_header(
+    tmpdir_factory, srcfiles: List[Path]
+):
+    dest = Path(tmpdir_factory.mktemp("input"))
+
+    # Copy files into an empty temporary directory
+    files = set()
+    for srcfile in srcfiles:
+        shutil.copy(srcfile, dest)
+        files.add(dest / srcfile.name)
+
+    # Run image builder
+    result = _build_files(
+        builder=image_builder_nrrd,
+        files=files,
+        output_directory=tmpdir_factory.mktemp("output"),
+    )
+
+    # Verify results (should not read anything)
+    assert result.consumed_files == set()
+    assert len(result.new_images) == 0
+
+
+def test_image_builder_with_unsupported_version(tmpdir):
+    dest = Path(tmpdir) / "image10x11x12_unsupported_version.nrrd"
+    shutil.copy(RESOURCE_PATH / "nrrd" / dest.name, dest)
+    files = {Path(d[0]).joinpath(f) for d in os.walk(tmpdir) for f in d[2]}
+    with TemporaryDirectory() as output:
+        result = _build_files(
+            builder=image_builder_nrrd, files=files, output_directory=output
+        )
+    assert result.consumed_files == set()
+    assert len(result.new_images) == 0
 
 
 def test_image_builder_with_other_file_extension(tmpdir):
