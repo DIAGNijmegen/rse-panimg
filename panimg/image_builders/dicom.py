Fix spacing computation for 4D images

panimg/image_builders/dicom.py
@@ -222,7 +222,14 @@ def _process_dicom_file(*, dicom_ds):  # noqa: C901
         # Use the dot product to find the angle between that direction
         # and the spacing vector - this tells us whether the order of
         # the slices is correct or should be reversed
-        z_order = np.sign(np.dot(avg_origin_diff, direction @ (1, 1, 1)))
+        z_direction = direction @ np.ones(dimensions)
+        if dimensions > 3:
+            avg_origin_diff = np.pad(
+                avg_origin_diff,
+                pad_width=(0, dimensions - avg_origin_diff.size),
+                mode="constant",
+            )
+        z_order = np.sign(np.dot(avg_origin_diff, z_direction))
 
     samples_per_pixel = int(getattr(ref_file, "SamplesPerPixel", 1))
     img = _create_itk_from_dcm(