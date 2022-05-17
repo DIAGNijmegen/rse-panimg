20522043b9058a3841240a20cc19719039849331
Chris <chris.vanrun@radboudumc.nl>
2022-05-17 03:55:49 +0200

    Move MHA value range computation from a post-processor to a model validator

--- panimg/post_processors/__init__.py
+++ panimg/post_processors/__init__.py
@@ -1,6 +1,5 @@
-from panimg.post_processors.mha_pixel_range import mha_pixel_range
 from panimg.post_processors.tiff_to_dzi import tiff_to_dzi
 
-DEFAULT_POST_PROCESSORS = [tiff_to_dzi, mha_pixel_range]
+DEFAULT_POST_PROCESSORS = [tiff_to_dzi]
 
-__all__ = ["tiff_to_dzi", "mha_pixel_range", "DEFAULT_POST_PROCESSORS"]
+__all__ = ["tiff_to_dzi", "DEFAULT_POST_PROCESSORS"]
